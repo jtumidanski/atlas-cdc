1b5e4a5dcf7be95f533e50f3ae324c2f03b5906c
jtumidanski <jtumidanski@gmail.com>
2021-11-28 16:20:54 -0500

    Add Marshal functions to support logging REST request responses.

--- atlas.com/cdc/rest/attributes/monster.go
+++ atlas.com/cdc/rest/attributes/monster.go
@@ -1,61 +1,78 @@
 package attributes
 
-import "atlas-cdc/rest/response"
+import (
+	"atlas-cdc/rest/response"
+	"encoding/json"
+)
 
 type MonsterDataContainer struct {
-   data response.DataSegment
+	data     response.DataSegment
+	included response.DataSegment
 }
 
 type MonsterData struct {
-   Id         string            `json:"id"`
-   Type       string            `json:"type"`
-   Attributes MonsterAttributes `json:"attributes"`
+	Id         string            `json:"id"`
+	Type       string            `json:"type"`
+	Attributes MonsterAttributes `json:"attributes"`
 }
 
 type DamageEntry struct {
-   CharacterId uint32 `json:"characterId"`
-   Damage      int64  `json:"damage"`
+	CharacterId uint32 `json:"characterId"`
+	Damage      int64  `json:"damage"`
 }
 
 type MonsterAttributes struct {
-   WorldId            byte          `json:"worldId"`
-   ChannelId          byte          `json:"channelId"`
-   MapId              uint32        `json:"mapId"`
-   MonsterId          uint32        `json:"monsterId"`
-   ControlCharacterId uint32        `json:"controlCharacterId"`
-   X                  int16         `json:"x"`
-   Y                  int16         `json:"y"`
-   FH                 int16        `json:"fh"`
-   Stance             byte          `json:"stance"`
-   Team               int8          `json:"team"`
-   HP                 uint32        `json:"hp"`
-   DamageEntries      []DamageEntry `json:"damageEntries"`
+	WorldId            byte          `json:"worldId"`
+	ChannelId          byte          `json:"channelId"`
+	MapId              uint32        `json:"mapId"`
+	MonsterId          uint32        `json:"monsterId"`
+	ControlCharacterId uint32        `json:"controlCharacterId"`
+	X                  int16         `json:"x"`
+	Y                  int16         `json:"y"`
+	FH                 int16         `json:"fh"`
+	Stance             byte          `json:"stance"`
+	Team               int8          `json:"team"`
+	HP                 uint32        `json:"hp"`
+	DamageEntries      []DamageEntry `json:"damageEntries"`
+}
+
+func (c *MonsterDataContainer) MarshalJSON() ([]byte, error) {
+	t := struct {
+		Data     interface{} `json:"data"`
+		Included interface{} `json:"included"`
+	}{}
+	if len(c.data) == 1 {
+		t.Data = c.data[0]
+	} else {
+		t.Data = c.data
+	}
+	return json.Marshal(t)
 }
 
 func (c *MonsterDataContainer) UnmarshalJSON(data []byte) error {
-   d, _, err := response.UnmarshalRoot(data, response.MapperFunc(EmptyMonsterData))
-   if err != nil {
-      return err
-   }
-   c.data = d
-   return nil
+	d, _, err := response.UnmarshalRoot(data, response.MapperFunc(EmptyMonsterData))
+	if err != nil {
+		return err
+	}
+	c.data = d
+	return nil
 }
 
 func (c *MonsterDataContainer) Data() *MonsterData {
-   if len(c.data) >= 1 {
-      return c.data[0].(*MonsterData)
-   }
-   return nil
+	if len(c.data) >= 1 {
+		return c.data[0].(*MonsterData)
+	}
+	return nil
 }
 
 func (c *MonsterDataContainer) DataList() []MonsterData {
-   var r = make([]MonsterData, 0)
-   for _, x := range c.data {
-      r = append(r, *x.(*MonsterData))
-   }
-   return r
+	var r = make([]MonsterData, 0)
+	for _, x := range c.data {
+		r = append(r, *x.(*MonsterData))
+	}
+	return r
 }
 
 func EmptyMonsterData() interface{} {
-   return &MonsterData{}
+	return &MonsterData{}
 }
